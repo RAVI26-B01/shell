5283def119eb731767702762889b33db87307a5d
RAVI26-B01 <ravi26.b01@gmail.com>
2020-04-20 19:39:40 +0530

    added new file

---
+++ employeeWage/addPartTime/addPartTime.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -x
+
+isPartTime=1;
+isFullTime=2
+empRatePerHour=20;
+randomCheck=$(( RANDOM%3 ))
+if [ $isFullTime -eq $randomCheck ];
+then 
+	empHours=8;
+
+elif [ $isPartTime -eq $randomCheck ];
+then
+	empHours=4;
+
+else
+	empHours=0;
+fi
+salary=$(( $empRatePerHour*$empHours ));
+
+
